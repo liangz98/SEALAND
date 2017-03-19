9078efe657be53ade4ce9e75c33d99dd3796dfa6
liangz98 <liangz98@qq.com>
2017-03-19 23:12:21 +0800

    配置 Laravel-Mix 使浏览器自动刷新  (npm run watch)

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,4 +14,6 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
+// 配置浏览器自动刷新, 入参为代理(即服务启动后的访问地址)
+// 使用命令 nmp run watch 命令会启动一个端口为 3000 的服务
 mix.browserSync('localhost:8000');
