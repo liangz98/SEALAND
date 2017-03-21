fbc039247c6b36c775179ebe4e37d111e8bf4c6e
liangz98 <liangz98@qq.com>
2017-03-21 17:09:39 +0800

    继续完成 Sea Land 相关表设计

--- database/migrations/2017_03_18_144013_create_sea_land_tables.php
+++ database/migrations/2017_03_18_144013_create_sea_land_tables.php
@@ -28,10 +28,11 @@ class CreateSeaLandTables extends Migration {
 		// 新闻
 		Schema::create('news', function (Blueprint $table) {
 			$table->increments('id');
-			$table->string('subject');
-			$table->text('content');
+			$table->text('subject');
+			$table->longText('content');
+			$table->timestamp('release_date')->nullable();		// 发布日期
 			$table->boolean('deleted')->default(false);			// 是否删除,default: false
-			$table->string('state', 2)->default('01');			// 状态,[01: 有效, 02: 禁用]
+			$table->string('state', 2)->default('01');			// 状态,[01: 有效, 02: 禁用, 03: 待发布]
 			$table->integer('created_by')->default(0);			// 创建人
 			$table->integer('last_updated_by')->default(0);		// 最后更新人
 			$table->timestamps();
@@ -40,7 +41,7 @@ class CreateSeaLandTables extends Migration {
 		// 培训类型
 		Schema::create('training_type', function (Blueprint $table) {
 			$table->increments('id');
-			$table->string('type_name');			// 分类名称
+			$table->string('name');					// 分类名称
 			$table->boolean('deleted')->default(false);			// 是否删除,default: false
 			$table->string('state', 2)->default('01');			// 状态,[01: 有效, 02: 禁用]
 			$table->integer('created_by')->default(0);			// 创建人
@@ -51,12 +52,17 @@ class CreateSeaLandTables extends Migration {
 		// 培训课程
 		Schema::create('training', function (Blueprint $table) {
 			$table->increments('id');
-			$table->integer('training_type_id');	// 分类ID
-			$table->string('name', 128);			// 课程名称
+			$table->integer('type_id');						// 分类ID
+			$table->string('name', 128);					// 课程名称
+			$table->string('number', 128)->nullable();		// 课程编号
+			$table->string('level', 128);					// 课程级别
+			$table->text('content')->nullable();			// 课程内容说明
+			$table->string('location', 128)->nullable();	// 培训地点
+			$table->text('address')->nullable();			// 详细地址
 			$table->timestamp('start_date')->nullable();		// 开始时间
 			$table->timestamp('end_date')->nullable();			// 结束时间
 			$table->boolean('deleted')->default(false);			// 是否删除,default: false
-			$table->string('state', 2)->default('01');			// 状态,[01: 有效, 02: 禁用]
+			$table->string('state', 2)->default('01');			// 状态,[01: 有效(接受报名), 02: 禁用]
 			$table->integer('created_by')->default(0);			// 创建人
 			$table->integer('last_updated_by')->default(0);		// 最后更新人
 			$table->timestamps();
@@ -65,22 +71,77 @@ class CreateSeaLandTables extends Migration {
 		// 会员培训申请 Register for a Course
 		Schema::create('register_course', function (Blueprint $table) {
 			$table->increments('id');
-			$table->integer('training_id');
+			$table->integer('member_id');			// 会员ID
+			$table->integer('training_id');			// 课程ID
 			$table->boolean('deleted')->default(false);			// 是否删除,default: false
-			$table->string('state', 2)->default('01');			// 状态,[01: 有效, 02: 禁用]
+			$table->string('state', 2)->default('02');			// 状态,[01: 审核通过, 02: 申请, 03: 拒绝, 04: 取消(或关闭)
+																// 05: 培训完成]
 			$table->integer('created_by')->default(0);			// 创建人
 			$table->integer('last_updated_by')->default(0);		// 最后更新人
 			$table->timestamps();
 		});
 
-		// 认证
+		// 认证(详情)
 		Schema::create('certification', function (Blueprint $table) {
+			$table->increments('id');					// 认证ID
+			$table->integer('member_id');				// 会员ID
+			$table->string('number', 128)->nullable();
+			$table->string('level', 128)->nullable();
 
+			$table->timestamp('start_date')->nullable();		// 发证日期
+			$table->timestamp('expiry_date')->nullable();		// 到期日期
+			$table->boolean('deleted')->default(false);			// 是否删除,default: false
+			$table->string('state', 2)->default('01');			// 状态,[01: 有效(接受报名), 02: 禁用]
+			$table->integer('created_by')->default(0);			// 创建人
+			$table->integer('last_updated_by')->default(0);		// 最后更新人
+			$table->timestamps();
 		});
 
-		// 会员认证/续订认证
+		// 会员认证/续订认证(申请)
 		Schema::create('renew_certification', function (Blueprint $table) {
-
+			$table->increments('id');	//
+			$table->integer('member_id');				// 会员ID
+			$table->string('number', 128)->nullable();
+			$table->string('level', 128)->nullable();
+			$table->string('first_name', 128)->nullable();
+			$table->string('last_name', 128)->nullable();
+			$table->time('birth_date')->nullable();
+			$table->string('user_name', 128)->nullable();
+			$table->string('user_password', 128)->nullable();
+			// 家庭地址信息
+			$table->text('person_address')->nullable();
+			$table->string('person_city', 64)->nullable();		// 城市
+			$table->string('person_province', 64)->nullable();	// 省份
+			$table->string('person_country', 64)->nullable();	// 国家
+			$table->string('person_zip_code', 64)->nullable();
+			$table->string('person_email', 64)->nullable();
+			$table->string('person_phone', 64)->nullable();
+			// 工作地址信息
+			$table->text('work_address')->nullable();
+			$table->string('work_city', 64)->nullable();		// 城市
+			$table->string('work_province', 64)->nullable();	// 省份
+			$table->string('work_country', 64)->nullable();		// 国家
+			$table->string('work_zip_code', 64)->nullable();
+			$table->string('work_email', 64)->nullable();
+			$table->string('work_phone', 64)->nullable();
+			$table->string('work_type', 64)->nullable();		// 工作类型
+			// 工作经历信息
+			$table->text('company_name')->nullable();			// 公司名称
+			$table->timestamp('from_date')->nullable();			// 开始年月
+			$table->timestamp('to_date')->nullable();			// 结束年月(在职留空)
+			$table->text('company_address')->nullable();		// 公司地址
+			$table->string('company_city', 64)->nullable();		// 城市
+			$table->string('company_province', 64)->nullable();	// 省份
+			$table->string('company_country', 64)->nullable();	// 国家
+			$table->string('company_zip_code', 64)->nullable();
+			$table->string('title', 64)->nullable();			// 职务
+			$table->text('description')->nullable();			// 工作描述
+			// 证明人信息
+			$table->boolean('deleted')->default(false);			// 是否删除,default: false
+			$table->string('state', 2)->default('01');			// 状态,[01: 有效(接受报名), 02: 禁用]
+			$table->integer('created_by')->default(0);			// 创建人
+			$table->integer('last_updated_by')->default(0);		// 最后更新人
+			$table->timestamps();
 		});
 
 		// 会员
@@ -124,7 +185,7 @@ class CreateSeaLandTables extends Migration {
 			$table->timestamps();
 		});
 
-		// 会员资格
+		// 会员资格/会员续期
 		Schema::create('membership', function (Blueprint $table){
 			$table->increments('id');
 			$table->integer('member_id')->default(0);
